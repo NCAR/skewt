a67e172507132ca27a0fd9bfb024a2c693008b36
philxu <philxu@20718509-2204-0410-96f3-d744d37ecd40>
2011-04-18 18:15:35 +0000

    Apply heuristics to pressure lines when SkewT graph is zoomed.

--- IsoPress.cpp
+++ IsoPress.cpp
@@ -24,39 +24,44 @@ using namespace skewt;
 //////////////////////////////////////////////////////////////////////
 // Construction/Destruction
 //////////////////////////////////////////////////////////////////////
-
 IsoPress::IsoPress(SkewTAdapter& adapter,
                    double xmin, double xmax, double ymin, double ymax, 
                    double tmin, double tmax, double pmin, double pmax,
                    double tSlope):
 IsoPleth(adapter, xmin, xmax, ymin, ymax, tmin, tmax, pmin, pmax, tSlope)
-  {
-  
-  }
+{
+}
 
 //////////////////////////////////////////////////////////////////////
 IsoPress::~IsoPress()
-  {
-  
-  }
+{
+}
 
 //////////////////////////////////////////////////////////////////////
 void IsoPress::draw()
-  {
+{
   unsigned int color = SKEWT_RED;
   
   double tmin = t(0.0, 1.0);
-  double p = floor(m_pmin/10.0)*10.0;
+  double pLine, pStep;
+  double pDelta = m_pmax - m_pmin;
+  if (pDelta > 400.0)
+    pStep = 100.0;
+  else if (pDelta > 150.0)
+    pStep = 50.0;
+  else if (pDelta > 50.0)
+    pStep = 20.0;
+  else if (pDelta > 10.0)
+    pStep = 10.0;
+  else
+    pStep = 1.0;
   
-  while (p <= m_pmax)
-    {
-    drawLine(tmin, p, m_tmax, p, color);
+  pLine = floor(m_pmin/pStep)*pStep;
+  while (pLine <= m_pmax) {
+    drawLine(tmin, pLine, m_tmax, pLine, color);
     char label[100];
-    sprintf(label, "|r%4.0f", p);
-    Text(label, -0.03, p, -0.01, color);
-    if (p < 100.0)
-        p += 10.0;
-    else
-        p += 100.0;
+    sprintf(label, "|r%4.0f", pLine);
+    Text(label, -0.03, pLine, -0.01, color);
+    pLine += pStep;
   }
 }
