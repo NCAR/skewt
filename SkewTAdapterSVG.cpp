e45e453d361c2054d4951ecbee1dcf02418a2e1c
Isabel Suhr <isabels@ucar.edu>
2019-01-31 10:00:29 -0800

    Fix a compiler warning - delete unused variables.

--- SkewTAdapterSVG.cpp
+++ SkewTAdapterSVG.cpp
@@ -53,13 +53,6 @@ void SkewTAdapterSVG::line(double x1,
     // one segment matches the beginning of the next segment, the segments are consolidated into 
     // a single polyline.
 
-    double x[2];
-    double y[2];
-    x[0] = x1;
-    x[1] = x2;
-    y[0] = y1;
-    y[1] = y2;
-
     if (_firstLineCall) {
         _firstLineCall = false;
         _xvals.push_back(x1);
