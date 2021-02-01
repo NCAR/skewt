7b9594ee83bad3c87d2e9230effadc054adc85f7
Gary Granger <granger@ucar.edu>
2021-02-01 09:15:15 -0700

    link to library directly, force qualified headers

--- tool_skewt.py
+++ tool_skewt.py
@@ -1,63 +1,61 @@
 toolname = 'skewt'
 
-thisDir = Dir('.').abspath
-upDir   = Dir('./../').abspath
-
-# Define tool. This must match the value of toolname.
-def skewt(env):
-    env.Append(LIBS = [toolname])
-    env.AppendUnique(CPPPATH = [upDir])
-    env.AppendUnique(CPPPATH = [thisDir])
-    env.AppendUnique(LIBPATH = [thisDir])
-    env.Require(['prefixoptions'])
-    
-Export(toolname)
-
-# Build library
 sources = Split("""
-   Adiabats.cpp
-   IsoMR.cpp
-   IsoPleth.cpp
-   IsoTherms.cpp
-   IsoPress.cpp
-   PAdiabats.cpp
-   PresRect.cpp
-   Rect.cpp
-   SkewT.cpp
-   SkewTAdapter.cpp
-   SkewTMetFormulas.cpp
-   SkewTRect.cpp
-   SkewTtrace.cpp
-   WindBarb.cpp
-   WindBarbs.cpp
-   SkewTAdapterSVG.cpp
+    Adiabats.cpp
+    IsoMR.cpp
+    IsoPleth.cpp
+    IsoTherms.cpp
+    IsoPress.cpp
+    PAdiabats.cpp
+    PresRect.cpp
+    Rect.cpp
+    SkewT.cpp
+    SkewTAdapter.cpp
+    SkewTMetFormulas.cpp
+    SkewTRect.cpp
+    SkewTtrace.cpp
+    WindBarb.cpp
+    WindBarbs.cpp
+    SkewTAdapterSVG.cpp
 """)
 
 headers = Split("""
-   Adiabats.h
-   IsoMR.h
-   IsoPleth.h
-   IsoPress.h
-   IsoTherms.h
-   PAdiabats.h
-   PresRect.h
-   Rect.h
-   SkewT.h
-   SkewTAdapter.h
-   SkewTAdapterSVG.h
-   SkewTMetFormulas.h
-   SkewTRect.h
-   SkewTdefs.h
-   SkewTdoxygen.h
-   SkewTtrace.h
-   WindBarb.h
-   WindBarbs.h
+    Adiabats.h
+    IsoMR.h
+    IsoPleth.h
+    IsoPress.h
+    IsoTherms.h
+    PAdiabats.h
+    PresRect.h
+    Rect.h
+    SkewT.h
+    SkewTAdapter.h
+    SkewTAdapterSVG.h
+    SkewTMetFormulas.h
+    SkewTRect.h
+    SkewTdefs.h
+    SkewTdoxygen.h
+    SkewTtrace.h
+    WindBarb.h
+    WindBarbs.h
 """)
 
-env = Environment(tools = ['default', 'doxygen', toolname])
+env = Environment(tools=['default', 'doxygen'])
 
 lib = env.Library(toolname, sources)
 env.Default(lib)
 
-# Create doxygen
-doxref = env.Apidocs(sources + headers, DOXYFILE_DICT={'PROJECT_NAME':toolname, 'PROJECT_NUMBER':'1.0'})
+doxref = env.Apidocs(sources + headers,
+                     DOXYFILE_DICT={'PROJECT_NAME': toolname})
+
+tooldir = env.Dir('.')
+
+
+def skewt(env):
+    env.Append(LIBS=[lib])
+    # Headers should be included qualified by the directory name, so make
+    # sure the parent directory is on CPPPATH.
+    env.AppendUnique(CPPPATH=[tooldir.Dir('..')])
+
+
+Export(toolname)
